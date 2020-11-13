e3f06aa42087182e9781c643951b41d0484e3045
Kamilla Kenzhekhankyzy <kamilla.kenzhekhankyzy@nu.edu.kz>
2020-11-14 01:30:32 +0600

    style(login-page): the layout of login page styled

--- src/pages/HomePage/component.tsx
+++ src/pages/HomePage/component.tsx
@@ -4,10 +4,12 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { is } from 'remote-data-ts';
 import classNames from 'classnames';
 import './index.scss';
-
 import { FormValues, FormInputs } from './types/FormData';
 import { atoms, actions } from 'store/auth';
 import { useHistory } from 'react-router-dom';
+import fireGreen from 'assets/images/fireGreen.png';
+import lightBulb from 'assets/images/lightBulb.png';
+import present from 'assets/images/present.png';
 
 export const HomePage: FC = () => {
   const history = useHistory();
@@ -38,50 +40,81 @@ export const HomePage: FC = () => {
 
   return (
     <div className="home-page">
-      <h1> Домашняя страница </h1>
-      <div className="auth-form">
-        <span style={{ fontWeight: 500, marginBottom: 32 }} className="title mb-32">
-          Аuthorization
-        </span>
-        <form onSubmit={handleSubmit(onFormSubmit)}>
-          <div className="input-wrp mt-64">
-            <span className="">Username</span>
-            <input
-              name={FormInputs.Username}
-              id={FormInputs.Username}
-              type="text"
-              className="inputText typography__variant-text my-8 mr-8"
-              ref={register({
-                required: 'Required',
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: 'Invalid email pattern',
-                },
-              })}
-              placeholder="Enter username"
-              required
-            />
+      {/* <h1> Домашняя страница </h1> */}
+      <div className="auth d-flex">
+        <div className="auth-left d-flex justify-content-center align-items-center">
+          <div className="auth-left_content d-flex justify-content-center px-16">
+            <div className="types-div d-flex flex-column">
+              <span className="types-text d-flex align-self-center" style={{ fontWeight: 500 }}>
+                Boost enthusiasm in your team&apos;s work by
+              </span>
+              <div className="types-list">
+                <div className="types-item d-flex mt-24 mb-32">
+                  <img src={lightBulb} alt="light" width="33px" height="33px" />
+                  <span className="item-text align-self-center ml-24">
+                    encouraging creative ideas
+                  </span>
+                </div>
+                <div className="types-item d-flex mb-32">
+                  <img src={present} alt="present" width="33px" height="33px" />
+                  <span className="item-text align-self-center ml-24">
+                    exchanging bonuses for prizes
+                  </span>
+                </div>
+                <div className="types-item d-flex mb-32">
+                  <img src={fireGreen} alt="fire" width="33px" height="33px" />
+                  <span className="item-text align-self-center ml-24"> tracing performance</span>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="input-wrp" style={{ marginTop: '24px' }}>
-            <span className="">Password</span>
-            <input
-              name={FormInputs.Password}
-              id={FormInputs.Password}
-              type="password"
-              className="inputText typography__variant-text my-8"
-              placeholder="Enter password"
-              required
-              ref={register()}
-            />
+        </div>
+        <div className="auth-right">
+          <div className="auth-form">
+            <span style={{ fontWeight: 500, marginBottom: 32 }} className="title">
+              Аuthorization
+            </span>
+            <form onSubmit={handleSubmit(onFormSubmit)}>
+              <div className="input-wrp mt-24">
+                <span className="">Username</span>
+                <input
+                  name={FormInputs.Username}
+                  id={FormInputs.Username}
+                  type="text"
+                  className="inputText typography__variant-text my-8 mr-8"
+                  ref={register({
+                    required: 'Required',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: 'Invalid email pattern',
+                    },
+                  })}
+                  placeholder="Enter username"
+                  required
+                />
+              </div>
+              <div className="input-wrp" style={{ marginTop: '24px' }}>
+                <span className="">Password</span>
+                <input
+                  name={FormInputs.Password}
+                  id={FormInputs.Password}
+                  type="password"
+                  className="inputText typography__variant-text my-8"
+                  placeholder="Enter password"
+                  required
+                  ref={register()}
+                />
+              </div>
+              <button
+                className={classNames(['auth_button typography__variant-subtext'])}
+                style={{ marginTop: '32px' }}
+                type="submit"
+              >
+                Login
+              </button>
+            </form>
           </div>
-          <button
-            className={classNames(['auth_button typography__variant-subtext'])}
-            style={{ marginTop: '32px' }}
-            type="submit"
-          >
-            Login
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
